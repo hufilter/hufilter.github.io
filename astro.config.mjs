4f8fd9cc5f6c999bb8c8b0762e2b4b4010a6a83d
scripthunter7 <scripthunter7@protonmail.com>
2024-07-12 22:47:08 +0200

    Add a temporary site content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import { url } from './src/constants';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hufilter.hu',
+  site: url,
   integrations: [tailwind()]
 });

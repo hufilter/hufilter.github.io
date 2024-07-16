d38a3481355f917311abbafb706bfdf766b91377
scripthunter7 <scripthunter7@protonmail.com>
2024-07-16 21:18:52 +0200

    Improve main site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
+import { AdBlockLogo, AdblockPlusLogo, AdBlockUltimateLogo, AdGuardLogo, BraveLogo, PiHoleLogo, RethinkDNSLogo, UBlockOriginLogo } from '../components/LogoProvider';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -18,6 +19,49 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+
+        <div className={clsx('flex flex-wrap justify-center gap-2 md:gap-3 my-6')}>
+          <div className={clsx('flex justify-center gap-1')}>
+            <AdGuardLogo />
+            <span>AdGuard</span>
+          </div>
+
+          <div className={clsx('flex justify-center gap-1')}>
+            <UBlockOriginLogo />
+            <span>uBlock Origin</span>
+          </div>
+
+          <div className={clsx('flex justify-center gap-1')}>
+            <BraveLogo />
+            <span>Brave</span>
+          </div>
+
+          <div className={clsx('flex justify-center gap-1')}>
+            <AdBlockUltimateLogo />
+            <span>AdBlocker Ultimate</span>
+          </div>
+
+          <div className={clsx('flex justify-center gap-1')}>
+            <AdBlockLogo />
+            <span>AdBlock</span>
+          </div>
+
+          <div className={clsx('flex justify-center gap-1')}>
+            <AdblockPlusLogo />
+            <span>Adblock Plus</span>
+          </div>
+
+          <div className={clsx('flex justify-center gap-1')}>
+            <PiHoleLogo />
+            <span>Pi-hole</span>
+          </div>
+
+          <div className={clsx('flex justify-center gap-1')}>
+            <RethinkDNSLogo />
+            <span>RethinkDNS</span>
+          </div>
+        </div>
+
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
@@ -35,7 +79,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Hufilter - A magyar reklámszűrő lista">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
